a641ce564e6c7aede21f286e51dbfe5e403bc811
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-06-12 15:01:42 +0300

    feat: add solid task

--- src/Components/Main/Section/Profile/Section/MyPosts/MyPosts.tsx
+++ src/Components/Main/Section/Profile/Section/MyPosts/MyPosts.tsx
@@ -10,9 +10,9 @@ export const MyPosts = () => {
             My posts
             <div>New Post</div>
             <div className={mp.posts}>
-                <Post message="Hi, hoe are you?!!"/>
-                <Post message="It's my first post "/>
-                <Post message="My name stas"/>
+                <Post message="Hi, hoe are you?!!" like="15"/>
+                <Post message="It's my first post" like="20"/>
+
             </div>
         </div>
     );
--- src/Components/Main/Section/Profile/Section/MyPosts/Post/Post.tsx
+++ src/Components/Main/Section/Profile/Section/MyPosts/Post/Post.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import post from "./Post.module.css"
 type PostType = {
     message: string
+    like: string
 }
 
 export const Post = (props: PostType) => {
@@ -9,7 +10,7 @@ export const Post = (props: PostType) => {
         <div className={post.item}>
             <img src="https://cdn.pixabay.com/photo/2023/05/17/08/55/tree-7999477__340.jpg" alt="post logo"/>
             {props.message}
-            <span>like</span>
+            <span>like {props.like}</span>
         </div>
     );
 };
