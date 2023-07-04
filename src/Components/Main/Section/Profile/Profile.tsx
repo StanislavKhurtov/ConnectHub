570b7b2e978a22dd19863db5a0ee420e56c8d8ac
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-04 11:05:18 +0300

    feat: add lesson #23

--- src/Components/Main/Section/Profile/Profile.tsx
+++ src/Components/Main/Section/Profile/Profile.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {Image} from "./Section/Image/Image";
 import {Avatar} from "./Section/Avatar/Avatar";
 import {MyPosts} from "./Section/MyPosts/MyPosts";
+import profile from './Profile.module.css'
 
 export const Profile = () => {
     return (
-        <div>
+        <div className={profile.body}>
             <Image/>
             <Avatar/>
             <MyPosts/>
