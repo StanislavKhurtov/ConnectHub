b1d505589f8a9f00d553053c18ce49075ea84ae0
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-08 16:28:16 +0300

    feat: add lesson #28 data transfer

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,24 @@ import {Header} from "./Components/Header/Header";
 import {Main} from "./Components/Main/Main";
 import {Footer} from "./Components/Footer/Footer";
 import {BrowserRouter} from "react-router-dom";
+import {DialogsDataType, MessagesDataType, PostType} from "./index";
 
+type AppType = {
+    posts: Array<PostType>
+    dialogsData: Array<DialogsDataType>
+    messagesData: Array<MessagesDataType>
+}
 
-const App = () => {
+const App = (props: AppType) => {
+
     return (
         <BrowserRouter>
             <div className="app-wrapper">
                 <Header/>
-                <Main/>
+                <Main
+                    posts={props.posts}
+                    dialogsData={props.dialogsData}
+                    messagesData={props.messagesData}/>
                 <Footer/>
             </div>
         </BrowserRouter>
