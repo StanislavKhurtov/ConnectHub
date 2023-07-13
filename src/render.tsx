8b5d88d3f6a60f1f6c88dac2b51d0335437a2a96
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-13 14:01:14 +0300

    feat: add lesson #34 main

--- src/render.tsx
+++ src/render.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addPost, StateProps} from "./Redux/State";
+import {addPost, StateProps, updatePostText} from "./Redux/State";
 import {BrowserRouter} from "react-router-dom";
 
 
 export let rerenderEntireTree = (state:StateProps) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost}/>
+            <App
+                state={state}
+                addPost={addPost}
+                updatePostText={updatePostText}
+            />
         </BrowserRouter>,
         document.getElementById('root'
         )
