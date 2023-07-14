b497a33b96fc4ad5e49a1291222240a2f730d9de
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-14 19:23:38 +0300

    refactor: add lesson #37 change structure state.tsx

--- src/Redux/State.tsx
+++ src/Redux/State.tsx
@@ -1,41 +1,3 @@
-let store = {
-   state: {
-        profilePage: {
-            posts: [
-                {id: 1, message: "Hi", likesCount: 15},
-                {id: 2, message: "How is your it-kamasutra", likesCount: 14},
-                {id: 3, message: "yo", likesCount: 9},
-                {id: 4, message: "полный пипец", likesCount: 10},
-                {id: 5, message: "Hi", likesCount: 25},
-            ],
-            newPostText: '',
-        },
-        dialogsPage: {
-            dialogs: [
-                {id: 1, name: "Stanislav"},
-                {id: 1, name: "Egor"},
-                {id: 1, name: "Sofia"},
-                {id: 1, name: "Natalia"},
-                {id: 1, name: "Alexey"},
-                {id: 1, name: "Artur"},
-            ],
-            messages: [
-                {id: 1, message: "Hi"},
-                {id: 2, message: "How is your it-kamasutra"},
-                {id: 3, message: "yo"},
-                {id: 4, message: "полный пипец"},
-                {id: 5, message: "Hi"},
-            ],
-        },
-
-    },
-}
-
-
-let rerenderEntireTree = (state: StateProps) => {
-    console.log('State was changed')
-}
-
 export type PostType = {
     id: number
     message: string
@@ -65,54 +27,64 @@ type DialogsPageType = {
     messages: MessagesDataType[]
 }
 
-export let state: StateProps = {
-    profilePage: {
-        posts: [
-            {id: 1, message: "Hi", likesCount: 15},
-            {id: 2, message: "How is your it-kamasutra", likesCount: 14},
-            {id: 3, message: "yo", likesCount: 9},
-            {id: 4, message: "полный пипец", likesCount: 10},
-            {id: 5, message: "Hi", likesCount: 25},
-        ],
-        newPostText: '',
+export let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: "Hi", likesCount: 15},
+                {id: 2, message: "How is your it-kamasutra", likesCount: 14},
+                {id: 3, message: "yo", likesCount: 9},
+                {id: 4, message: "полный пипец", likesCount: 10},
+                {id: 5, message: "Hi", likesCount: 25},
+            ],
+            newPostText: '',
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: "Stanislav"},
+                {id: 1, name: "Egor"},
+                {id: 1, name: "Sofia"},
+                {id: 1, name: "Natalia"},
+                {id: 1, name: "Alexey"},
+                {id: 1, name: "Artur"},
+            ],
+            messages: [
+                {id: 1, message: "Hi"},
+                {id: 2, message: "How is your it-kamasutra"},
+                {id: 3, message: "yo"},
+                {id: 4, message: "полный пипец"},
+                {id: 5, message: "Hi"},
+            ],
+        },
+
     },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: "Stanislav"},
-            {id: 1, name: "Egor"},
-            {id: 1, name: "Sofia"},
-            {id: 1, name: "Natalia"},
-            {id: 1, name: "Alexey"},
-            {id: 1, name: "Artur"},
-        ],
-        messages: [
-            {id: 1, message: "Hi"},
-            {id: 2, message: "How is your it-kamasutra"},
-            {id: 3, message: "yo"},
-            {id: 4, message: "полный пипец"},
-            {id: 5, message: "Hi"},
-        ],
+    rerenderEntireTree(state: StateProps) {
+        console.log('State changed')
     },
+    addPost() {
+        let newPost = {id: 5, message: state.profilePage.newPostText, likesCount: 0};
+        state.profilePage.posts.push(newPost);
+        rerenderEntireTree(state);
+    },
+    updatePostText(newText: string) {
+        state.profilePage.newPostText = newText;
+        rerenderEntireTree(state);
+    },
+    addMessage(message: string) {
+        let newMessage = {id: 6, message: message};
+        state.dialogsPage.messages.push(newMessage);
+        rerenderEntireTree(state);
+    },
+    subscrube(observer: any) {
+        rerenderEntireTree = observer;
+    }
 
-};
-
-export const addPost = () => {
-    let newPost = {id: 5, message: state.profilePage.newPostText, likesCount: 0};
-    state.profilePage.posts.push(newPost);
-    rerenderEntireTree(state);
 }
 
-export const updatePostText = (newText: string) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
 
-export const addMessage = (message: string) => {
-    let newMessage = {id: 6, message: message};
-    state.dialogsPage.messages.push(newMessage);
-    rerenderEntireTree(state);
-}
 
-export const subscrube = (observer: any) => {
-    rerenderEntireTree = observer; //наблюдатель pattern // publisher-subscruber
-}
+
+
+
+
+
