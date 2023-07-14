9b92980a3eb27291fb508d2019552ffd8256e069
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-14 19:17:20 +0300

    feat: add lesson #37 add store

--- src/Redux/State.tsx
+++ src/Redux/State.tsx
@@ -1,3 +1,37 @@
+let store = {
+   state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: "Hi", likesCount: 15},
+                {id: 2, message: "How is your it-kamasutra", likesCount: 14},
+                {id: 3, message: "yo", likesCount: 9},
+                {id: 4, message: "полный пипец", likesCount: 10},
+                {id: 5, message: "Hi", likesCount: 25},
+            ],
+            newPostText: '',
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: "Stanislav"},
+                {id: 1, name: "Egor"},
+                {id: 1, name: "Sofia"},
+                {id: 1, name: "Natalia"},
+                {id: 1, name: "Alexey"},
+                {id: 1, name: "Artur"},
+            ],
+            messages: [
+                {id: 1, message: "Hi"},
+                {id: 2, message: "How is your it-kamasutra"},
+                {id: 3, message: "yo"},
+                {id: 4, message: "полный пипец"},
+                {id: 5, message: "Hi"},
+            ],
+        },
+
+    },
+}
+
+
 let rerenderEntireTree = (state: StateProps) => {
     console.log('State was changed')
 }
