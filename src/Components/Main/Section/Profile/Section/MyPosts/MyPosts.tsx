107207624974ccadd1cca4a48c676550e81a6c54
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-23 11:25:29 +0300

    feat: add lesson #41 reducer change structure mypost

--- src/Components/Main/Section/Profile/Section/MyPosts/MyPosts.tsx
+++ src/Components/Main/Section/Profile/Section/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React, {RefObject} from 'react';
+import React, {ChangeEvent, MouseEvent} from 'react';
 import mp from './MyPost.module.css';
 import Post from "./Post/Post";
 import {PostType,addPostAC,updateNewPostAC} from "../../../../../../Redux/State";
@@ -10,19 +10,14 @@ type MyPostType = {
     dispatch: any
 }
 
-
 export const MyPosts = (props: MyPostType) => {
 
-    let newPostElement: RefObject<HTMLTextAreaElement> = React.createRef();
-
-    const addPost = () => {
-        if (newPostElement.current) {
+    const addPost = (e:MouseEvent<HTMLButtonElement>) => {
             props.dispatch(addPostAC());
-        }
     }
 
-    const onPostChange = () => {
-        let text = newPostElement.current?.value;
+    const onPostChange = (e: ChangeEvent<HTMLTextAreaElement> ) => {
+        let text = e.currentTarget.value;
         if (text) {
             props.dispatch(updateNewPostAC(text))
         }
@@ -33,7 +28,7 @@ export const MyPosts = (props: MyPostType) => {
             <div>My Post</div>
             <div>
                 <div>
-                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}/>
+                    <textarea value={props.newPostText} onChange={onPostChange} />
                 </div>
                 <div>
                     <button onClick={addPost}>Add post</button>
