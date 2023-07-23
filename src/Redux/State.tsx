e6465800895863ead4770fe8a2a263dcbbabd70f
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-23 13:32:20 +0300

    feat: lesson #41 reducer change structure state

--- src/Redux/State.tsx
+++ src/Redux/State.tsx
@@ -1,65 +1,10 @@
 import {v1} from "uuid";
+import {profileReducer} from "./profile-reducer";
+import {dialogsReducer} from "./dialogs-reducer";
+import {ActionsTypes, StateType, StoreType} from "./type";
 
 
-const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST = 'UPDATE-NEW-POST';
-const ADD_NEW_MESSAGE = 'ADD-MESSAGE';
 
-
-export type PostType = {
-    id: string
-    message: string
-    likesCount: number
-}
-export type DialogsDataType = {
-    id: string
-    name: string
-}
-
-export type MessagesDataType = {
-    id: string
-    message: string
-}
-
-export type StateProps = {
-    profilePage: ProfilePageType
-    dialogsPage: DialogsPageType
-}
-type ProfilePageType = {
-    posts: PostType[]
-    newPostText: string
-}
-
-type DialogsPageType = {
-    dialogs: DialogsDataType[]
-    messages: MessagesDataType[]
-}
-
-type StoreType = {
-    _state: StateProps
-    _callSubscriber: (state: StateProps) => void
-    getState: () => StateProps
-    subscribe: (callback: (state: StateProps) => void) => void
-    dispatch: (action: ActionsTypes) => void
-
-}
-
-
-export type AddPostActionType = {
-    type: 'ADD-POST'
-}
-
-export type UpdateNewPostTextActionType = {
-    type: 'UPDATE-NEW-POST'
-    newText: string
-}
-
-export type AddMessageActionType = {
-    type: 'ADD-MESSAGE'
-    message: string
-}
-
-export type ActionsTypes = AddPostActionType | UpdateNewPostTextActionType | AddMessageActionType;
 
 
 export let store: StoreType = {
@@ -94,7 +39,7 @@ export let store: StoreType = {
 
     },
 
-    _callSubscriber(state: StateProps) {
+    _callSubscriber(state: StateType) {
         console.log('State changed')
     },
 
@@ -106,50 +51,14 @@ export let store: StoreType = {
     },
 
     dispatch(action: ActionsTypes) {
-        switch (action.type) {
-            case ADD_POST: {
-                let newPost = {id: v1(), message: this._state.profilePage.newPostText, likesCount: 0};
-                let newProfilePage = {
-                    ...this._state.profilePage,
-                    posts: [...this._state.profilePage.posts, newPost],
-                    newPostText: ''
-                };
-                this._callSubscriber({...this._state, profilePage: newProfilePage});
-                break;
-            }
-            case UPDATE_NEW_POST: {
-                let newProfilePage = {...this._state.profilePage, newPostText: action.newText};
-                let newState = {...this._state, profilePage: newProfilePage};
-                this._state = newState;
-                this._callSubscriber(this._state);
-                break;
-            }
-
-            case ADD_NEW_MESSAGE: {
-                let newMessage = {id: v1(), message: action.message};
-                let newDialogsPage = {
-                    ...this._state.dialogsPage, messages: [...this._state.dialogsPage.messages, newMessage]
-                };
-                this._callSubscriber({...this._state, dialogsPage: newDialogsPage});
-                break;
-            }
-            default:
-                throw new Error("I don't understand this type")
-        }
-    },
+        this._state.profilePage = profileReducer(this._state.profilePage, action);
+        this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+        this._callSubscriber(this._state);
+    }
 }
 
-export const addPostAC = (): AddPostActionType => {
-    return {type: ADD_POST}
-}
 
-export const updateNewPostAC = (text: string): UpdateNewPostTextActionType => {
-    return {type: UPDATE_NEW_POST, newText: text}
-}
 
-export const addMessageActionCreator = (textMessage: string): AddMessageActionType => {
-    return {type: ADD_NEW_MESSAGE, message: textMessage}
-}
 
 
 
