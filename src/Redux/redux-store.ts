b5e7cca17c9994256f24ffcddb749c0b086dc54c
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-31 15:38:40 +0300

    feat: lesson #42 change structure redux-store

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -1,3 +1,12 @@
-import {createStore} from "redux";
+import {combineReducers, createStore} from "redux";
+import {profileReducer} from "./profile-reducer";
+import {dialogsReducer} from "./dialogs-reducer";
+import {sidebarReducer} from "./sidebar-reducer";
 
-let store = createStore();
+let reducers = combineReducers({
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    sidebar: sidebarReducer
+});
+
+export let store = createStore(reducers);
