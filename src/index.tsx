3f737d980cf4ee1dfae8f0e1a1e79d535df0bcff
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-31 14:33:47 +0300

    feat: lesson #42 rename State from store

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import {App} from './App';
 import {BrowserRouter} from "react-router-dom";
 import {StateType} from "./Redux/type";
-import {store} from "./Redux/State";
+import {store} from "./Redux/store";
 
 
 
