2b4b8b9bde681a4c6fbf64a4ffee0cbcc0cd5f8a
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-31 15:40:47 +0300

    feat: lesson #42 add initialState dialogs-reducer

--- src/Redux/dialogs-reducer.ts
+++ src/Redux/dialogs-reducer.ts
@@ -4,11 +4,29 @@ import {ActionsTypes, AddMessageActionType, DialogsPageType} from "./type";
 
 const ADD_MESSAGE = 'ADD-MESSAGE';
 
-export const dialogsReducer = (state: DialogsPageType, action: ActionsTypes): DialogsPageType => {
+const initialState = {
+    dialogs: [
+        {id: v1(), name: "Stanislav"},
+        {id: v1(), name: "Egor"},
+        {id: v1(), name: "Sofia"},
+        {id: v1(), name: "Natalia"},
+        {id: v1(), name: "Alexey"},
+        {id: v1(), name: "Artur"},
+    ],
+    messages: [
+        {id: v1(), message: "Hi"},
+        {id: v1(), message: "How is your it-kamasutra"},
+        {id: v1(), message: "yo"},
+        {id: v1(), message: "полный пипец"},
+        {id: v1(), message: "Hi"},
+    ]
+}
+
+export const dialogsReducer = (state: DialogsPageType = initialState, action: ActionsTypes): DialogsPageType => {
     switch (action.type) {
         case ADD_MESSAGE: {
-            const newMessage = { id: v1(), message: action.message };
-            return { ...state, messages: [...state.messages, newMessage] };
+            const newMessage = {id: v1(), message: action.message};
+            return {...state, messages: [...state.messages, newMessage]};
         }
         default:
             return state;
