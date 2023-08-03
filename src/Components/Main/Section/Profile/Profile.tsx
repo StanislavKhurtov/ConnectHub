2e17d111580e2c7edc1c46b2b696e0698a44276d
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-03 10:53:10 +0300

    fix : error file4

--- src/Components/Main/Section/Profile/Profile.tsx
+++ src/Components/Main/Section/Profile/Profile.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import {Image} from "./Section/Image/Image";
 import {Avatar} from "./Section/Avatar/Avatar";
-import {MyPosts} from "./Section/MyPosts/MyPosts";
 import profile from './Profile.module.css'
 import {PostType} from "../../../../Redux/type";
-
+import {MyPostsContainer} from "./Section/MyPosts/MyPostsContainer";
 
 
 type ProfileType = {
-    posts: PostType[]
-    newPostText: string
-    dispatch: any
+    store: any
 }
 
 export const Profile = (props: ProfileType) => {
@@ -19,10 +16,8 @@ export const Profile = (props: ProfileType) => {
         <div className={profile.body}>
             <Image/>
             <Avatar/>
-            <MyPosts
-                posts={props.posts}
-                newPostText={props.newPostText}
-                dispatch={props.dispatch}
+            <MyPostsContainer
+                store={props.store}
             />
         </div>
     );
