05416174c086a2259f1d7b44fad628d428ff3c25
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-10 20:10:40 +0300

    refactor: lesson #49 change structure main

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import main from './Main.module.css';
-import { Navbar } from './Section/Navbar/Navbar';
-import { Profile } from './Section/Profile/Profile';
-import { Route } from 'react-router-dom';
-import { News } from './Section/News/News';
-import { Settings } from './Section/Settings/Settings';
-import { Music } from './Section/Music/Music';
+import {Navbar} from './Section/Navbar/Navbar';
+import {Profile} from './Section/Profile/Profile';
+import {Route} from 'react-router-dom';
+import {News} from './Section/News/News';
+import {Settings} from './Section/Settings/Settings';
+import {Music} from './Section/Music/Music';
 import {DialogsContainer} from "./Section/Dialogs/DialogsContainer";
+import {UsersContainer} from "../Users/UsersContainer";
 
 
 export const Main = () => {
     return (
         <div className={main.main}>
             <div className={main.main__container}>
-                <Navbar />
+                <Navbar/>
                 <div className={main.body}>
-                    <Route path='/profile' render={() => <Profile />} />
-                    <Route path='/dialogs' render={() => <DialogsContainer />} />
-                    <Route path='/news' render={() => <News />} />
-                    <Route path='/music' render={() => <Music />} />
-                    <Route path='/settings' render={() => <Settings />} />
+                    <Route path='/profile' render={() => <Profile/>}/>
+                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                    <Route path='/users' render={() => <UsersContainer/>}/>
+                    <Route path='/news' render={() => <News/>}/>
+                    <Route path='/music' render={() => <Music/>}/>
+                    <Route path='/settings' render={() => <Settings/>}/>
                 </div>
             </div>
         </div>
