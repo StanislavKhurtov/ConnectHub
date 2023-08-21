0553740c57da200d506f4b1ab105e7ce2019d14e
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-21 11:58:52 +0300

    feat:lesson#55 change structure

--- src/Components/Users/UsersContainer.tsx
+++ src/Components/Users/UsersContainer.tsx
@@ -1,26 +1,38 @@
 import React from "react";
-import {connect} from 'react-redux';
-import {Dispatch} from "redux";
-import {Users} from "./Users";
-import {AppRootState} from "../../Redux/redux-store";
-import {followAC, setUsersAC, unFollowAC, UsersPageType} from "../../Redux/users-reducer";
-import UsersCl from "./UsersС";
-
-
+import { connect } from 'react-redux';
+import { Dispatch } from "redux";
+import { AppRootState } from "../../Redux/redux-store";
+import {
+    followAC,
+    setCurrentPageAC,
+    setUsersAC,
+    setUsersTotalCountAC,
+    unFollowAC,
+    UsersPageType
+} from "../../Redux/users-reducer";
+import {UsersC} from "./UsersС";
 
 type MapStateToPropsType = {
     users: Array<UsersPageType>
+    pageSize: number
+    totalCount: number
+    currentPage: number
 };
 
 type MapDispatchToPropsType = {
     follow: (userId: number) => void
     unFollow: (userId: number) => void
     setUsers: (users: Array<UsersPageType>) => void
+    setCurrent: (currentPage: number) => void
+    setTotalUsersCount: (totalCount: number) => void
 };
 
 const mapStateToProps = (state: AppRootState): MapStateToPropsType => {
     return {
-        users: state.usersPage.items
+        users: state.usersPage.items,
+        pageSize: state.usersPage.pageSize,
+        totalCount: state.usersPage.totalCount,
+        currentPage: state.usersPage.currentPage
     };
 };
 
@@ -34,8 +46,15 @@ const mapDispatchToProps = (dispatch: Dispatch): MapDispatchToPropsType => {
         },
         setUsers: (users: Array<UsersPageType>) => {
             dispatch(setUsersAC(users))
+        },
+        setCurrent: (currentPage: number) => {
+            dispatch(setCurrentPageAC(currentPage))
+        },
+        setTotalUsersCount: (totalCount: number) =>{
+            dispatch(setUsersTotalCountAC(totalCount))
         }
     };
 };
 
-export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersCl);
+export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersC);
+
