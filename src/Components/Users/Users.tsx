d82e9757e5d4dd46696d99e49359ca71500f6a39
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-21 14:12:53 +0300

    feat:lesson#56 change structure

--- src/Components/Users/Users.tsx
+++ src/Components/Users/Users.tsx
@@ -1,60 +1,86 @@
 import React from "react";
-import s from './users.module.css'
+import s from "./users.module.css";
 import {UsersPageType} from "../../Redux/users-reducer";
-import axios from "axios";
 import userPhoto from '../../assets/images/1.jpg'
 
 
-type UserType = {
-    users: Array<UsersPageType>
-    follow: (id: number) => void
-    unFollow: (userId: number) => void
-    setUsers: (users: Array<UsersPageType>) => void
-}
+type UsersTypeProps = {
+    users: Array<UsersPageType>;
+    follow: (id: number) => void;
+    unFollow: (userId: number) => void;
+    setUsers: (users: Array<UsersPageType>) => void;
+    setCurrent: (currentPage: number) => void;
+    setTotalUsersCount: (totalCount: number) => void;
+    pageSize: number;
+    totalCount: number;
+    currentPage: number;
+    onPageChanged: any
+};
 
+export const Users = (props: UsersTypeProps) => {
+    const {
+        users,
+        currentPage,
+        pageSize,
+        totalCount,
+        follow,
+        unFollow,
+    } = props;
 
-export const Users: React.FC<UserType> = (props) => {
-    let getUsers = () => {
+    const pageCount = Math.ceil(totalCount / pageSize);
+    const visiblePages = 10; // Количество видимых страниц
 
-        if (props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(response => {
-                    props.setUsers(response.data.items)
-                })
+    const startPage = Math.max(1, currentPage - Math.floor(visiblePages / 2));
+    const endPage = Math.min(pageCount, startPage + visiblePages - 1);
 
-        }
-
+    const pages = [];
+    for (let i = startPage; i <= endPage; i++) {
+        pages.push(i);
     }
-
-
 
     return (
         <div>
-            <button onClick={getUsers}>Get Users</button>
-            {props.users.map(el => <div key={el.id} className={s.wrapper}>
-                <div className={s.logo}>
-                    <div className={s.picture}>
-                        <img className={s.image} src={el.photos.small != null ? el.photos.small : userPhoto} alt="image logo"/>
+            {users.map((el) => (
+                <div key={el.id} className={s.wrapper}>
+                    <div className={s.logo}>
+                        <div className={s.picture}>
+                            <img
+                                src={el.photos.small != null ? el.photos.small : userPhoto}
+                                className={s.image}
+                                alt="image logo"
+                            />
+                        </div>
+                        <div>
+                            {el.followed ? (
+                                <button onClick={() => unFollow(el.id)}>Unfollow</button>
+                            ) : (
+                                <button onClick={() => follow(el.id)}>Follow</button>
+                            )}
+                        </div>
                     </div>
-                    <div>
-                        {el.followed ? (
-                            <button onClick={() => props.unFollow(el.id)}>Unfollow</button>
-                        ) : (
-                            <button onClick={() => props.follow(el.id)}>Follow</button>
-                        )}
+                    <div className={s.body}>
+                        <div>
+                            <div>{el.name}</div>
+                            <div>{el.status}</div>
+                        </div>
+                        <div>
+                            <div>{"Belarus"}</div>
+                            <div>{"Minsk"}</div>
+                        </div>
                     </div>
                 </div>
-                <div className={s.body}>
-                    <div>
-                        <div>{el.name}</div>
-                        <div>{el.status}</div>
-                    </div>
-                    <div>
-                        <div>{'el.location.country'}</div>
-                        <div>{'el.location.city'}</div>
-                    </div>
-                </div>
-            </div>)}
+            ))}
+            <div className={s.pagination}>
+                {pages.map((p) => (
+                    <span
+                        key={p}
+                        className={currentPage === p ? s.selectedPage : ""}
+                        onClick={() => props.onPageChanged(p)}
+                    >
+            {p}
+          </span>
+                ))}
+            </div>
         </div>
     );
-}
+};
