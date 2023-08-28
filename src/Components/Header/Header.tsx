6d0fce04982c7868fdb8cdda2e29fbfd6c7fed97
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-28 17:30:33 +0300

    feat: lesson#57 change structure header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import header from './Header.module.css';
-
+import logo from '../../assets/logo/logo.png'
 
 export const Header =()=> {
     return (
         <div className={header.header}>
-            <div className="header__container">
+            <div className={header.container}>
                 <div className={header.logo}>
                     <img className="image"
-                         src="https://global-uploads.webflow.com/5e157547d6f791d34ea4e2bf/6336fc5bc915769b7f4cfd60_Chronos-p-500.webp"
+                         src={logo}
                          alt="logo image"/>
                 </div>
             </div>
