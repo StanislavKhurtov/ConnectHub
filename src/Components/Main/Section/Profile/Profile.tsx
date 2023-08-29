faf565e3f70e18c67ea329e17f6cd5f7f4008fca
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-29 13:30:35 +0300

    feat: add lesson#59 change structure

--- src/Components/Main/Section/Profile/Profile.tsx
+++ src/Components/Main/Section/Profile/Profile.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
-import {Image} from "./Section/Image/Image";
-import {Avatar} from "./Section/Avatar/Avatar";
-import profile from './Profile.module.css'
-import {MyPostsContainer} from "./Section/MyPosts/MyPostsContainer";
+import { MyPostsContainer } from './Section/MyPosts/MyPostsContainer';
+import { ProfileInfo } from './Section/ProfileInfo/ProfileInfo';
 
+export interface ProfileType {
+    aboutMe: string;
+    contacts: {
+        facebook: string | null;
+        website: string | null;
+        vk: string | null;
+        twitter: string | null;
+        instagram: string | null;
+        youtube: string | null;
+        github: string | null;
+        mainLink: string | null;
+    };
+    lookingForAJob: boolean;
+    lookingForAJobDescription: string;
+    fullName: string;
+    userId: number;
+    photos: {
+        small: string;
+        large: string;
+    };
+}
 
+export type ProfileProps = {
+    profile: ProfileType | null;
+};
 
-export const Profile = () => {
-
+export const Profile: React.FC<ProfileProps> = ({ profile }) => {
     return (
-        <div className={profile.body}>
-            <Image/>
-            <Avatar/>
+        <div >
+            <ProfileInfo profile={profile} />
             <MyPostsContainer />
         </div>
     );
-}
-
-
+};
