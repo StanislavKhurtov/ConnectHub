7e5b6bc494c7a06f1d7d514934e05c8d8f3280c0
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-29 13:32:33 +0300

    feat: add lesson#59 change structure

--- src/Components/Main/Section/Profile/Section/Image/Image.tsx
+++ src/Components/Main/Section/Profile/Section/Image/Image.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import profile from "../../Profile.module.css";
+import photoHeader from '../../../../../../Image/12.webp'
 
 export const Image = () => {
     return(
         <div className={profile.content}>
             <div className={profile.picture}>
-                <img src="https://cdn.pixabay.com/photo/2023/05/17/08/55/tree-7999477__340.jpg"
-                     alt="content image"/>
+                <img src={photoHeader} alt="content image"/>
             </div>
 
         </div>
