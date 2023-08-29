b905cbdf544360895e6c97e46a69bb51f52fa983
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-29 13:32:25 +0300

    feat: add lesson#59 change structure

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import main from './Main.module.css';
-import {Navbar} from './Section/Navbar/Navbar';
-import {Profile} from './Section/Profile/Profile';
-import {Route} from 'react-router-dom';
-import {News} from './Section/News/News';
-import {Settings} from './Section/Settings/Settings';
-import {Music} from './Section/Music/Music';
-import {DialogsContainer} from "./Section/Dialogs/DialogsContainer";
-import {UsersContainer} from "../Users/UsersContainer";
-
+import { Navbar } from './Section/Navbar/Navbar';
+import { Route } from 'react-router-dom';
+import { News } from './Section/News/News';
+import { Settings } from './Section/Settings/Settings';
+import { Music } from './Section/Music/Music';
+import { DialogsContainer } from "./Section/Dialogs/DialogsContainer";
+import { UsersContainer } from "../Users/UsersContainer";
+import { ProfileContainer } from "./Section/Profile/ProfileContainer"; // Import the corrected component
 
 export const Main = () => {
     return (
         <div className={main.main}>
             <div className={main.main__container}>
-                <Navbar/>
+                <Navbar />
                 <div className={main.body}>
-                    <Route path='/profile' render={() => <Profile/>}/>
-                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
-                    <Route path='/users' render={() => <UsersContainer/>}/>
-                    <Route path='/news' render={() => <News/>}/>
-                    <Route path='/music' render={() => <Music/>}/>
-                    <Route path='/settings' render={() => <Settings/>}/>
+                    <Route path='/profile' component={ProfileContainer} />
+                    <Route path='/dialogs' component={DialogsContainer} />
+                    <Route path='/users' component={UsersContainer} />
+                    <Route path='/news' component={News} />
+                    <Route path='/music' component={Music} />
+                    <Route path='/settings' component={Settings} />
                 </div>
             </div>
         </div>
