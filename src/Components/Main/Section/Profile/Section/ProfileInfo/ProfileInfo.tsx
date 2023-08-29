11277a716c8b8b003ef2bdf8fb5f9aae692477ab
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-29 13:29:35 +0300

    feat: add lesson#59 change structure

---
+++ src/Components/Main/Section/Profile/Section/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Image } from "../Image/Image";
+import { Avatar } from "../Avatar/Avatar";
+import { Preloader } from "../../../../../common/Preloader/Preloader";
+import {ProfileType} from "../../Profile";
+
+
+type ProfileInfoProps = {
+    profile: ProfileType | null;
+};
+
+export const ProfileInfo: React.FC<ProfileInfoProps> = ({ profile }) => {
+    if (!profile) {
+        return <Preloader />;
+    }
+
+    return (
+        <>
+            <Image />
+            <Avatar profile={profile} />
+        </>
+    );
+};
