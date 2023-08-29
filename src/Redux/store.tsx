ff3db3484e7b2e92ad7ce6cabdbaaabefbe287a3
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-29 13:29:58 +0300

    feat: add lesson#59 change structure

--- src/Redux/store.tsx
+++ src/Redux/store.tsx
@@ -1,49 +1,50 @@
-import {v1} from "uuid";
-import {profileReducer} from "./profile-reducer";
-import {dialogsReducer} from "./dialogs-reducer";
-import {ActionsTypes, StateType, StoreType} from "./type";
-
+import { v1 } from "uuid";
+import { profileReducer } from "./profile-reducer";
+import { dialogsReducer } from "./dialogs-reducer";
+import { ActionsTypes, StateType, StoreType } from "./type";
 
 export let store: StoreType = {
     _state: {
         profilePage: {
             posts: [
-                {id: v1(), message: "Hi", likesCount: 15},
-                {id: v1(), message: "How is your it-kamasutra", likesCount: 14},
-                {id: v1(), message: "yo", likesCount: 9},
-                {id: v1(), message: "полный пипец", likesCount: 10},
-                {id: v1(), message: "Hi", likesCount: 25},
+                { id: v1(), message: "Hi", likesCount: 15 },
+                { id: v1(), message: "How is your it-kamasutra", likesCount: 14 },
+                { id: v1(), message: "yo", likesCount: 9 },
+                { id: v1(), message: "полный пипец", likesCount: 10 },
+                { id: v1(), message: "Hi", likesCount: 25 },
             ],
             newPostText: '',
+            profile: null, // Add a default value for the profile property
         },
         dialogsPage: {
             dialogs: [
-                {id: v1(), name: "Stanislav"},
-                {id: v1(), name: "Egor"},
-                {id: v1(), name: "Sofia"},
-                {id: v1(), name: "Natalia"},
-                {id: v1(), name: "Alexey"},
-                {id: v1(), name: "Artur"},
+                { id: v1(), name: "Stanislav" },
+                { id: v1(), name: "Egor" },
+                { id: v1(), name: "Sofia" },
+                { id: v1(), name: "Natalia" },
+                { id: v1(), name: "Alexey" },
+                { id: v1(), name: "Artur" },
             ],
             messages: [
-                {id: v1(), message: "Hi"},
-                {id: v1(), message: "How is your it-kamasutra"},
-                {id: v1(), message: "yo"},
-                {id: v1(), message: "полный пипец"},
-                {id: v1(), message: "Hi"},
+                { id: v1(), message: "Hi" },
+                { id: v1(), message: "How is your it-kamasutra" },
+                { id: v1(), message: "yo" },
+                { id: v1(), message: "полный пипец" },
+                { id: v1(), message: "Hi" },
             ],
         },
         sidebar: {},
     },
 
     _callSubscriber(state: StateType) {
-        console.log('State changed')
+        console.log('State changed');
     },
 
     getState() {
         return this._state;
     },
-    subscribe(observer: any) {
+
+    subscribe(observer: (state: StateType) => void) {
         this._callSubscriber = observer;
     },
 
@@ -52,21 +53,4 @@ export let store: StoreType = {
         this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
         this._callSubscriber(this._state);
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
