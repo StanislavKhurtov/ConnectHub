23893c208e78f046798aac06e663248bb72f1a83
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-29 13:30:13 +0300

    feat: add lesson#59 change structure

--- src/Redux/profile-reducer.ts
+++ src/Redux/profile-reducer.ts
@@ -1,10 +1,11 @@
 import {v1} from "uuid";
-import {ActionsTypes, AddPostActionType, ProfilePageType, UpdateNewPostTextActionType} from "./type";
+import {AddMessageActionType, AddPostActionType, ProfilePageType, UpdateNewPostTextActionType} from "./type";
 
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST = 'UPDATE-NEW-POST';
+const SET_USER_PROFILE = 'SET_USER_PROFILE';
 
-const initialState= {
+const initialState = {
     posts: [
         {id: v1(), message: "Hi", likesCount: 15},
         {id: v1(), message: "How is your it-kamasutra", likesCount: 14},
@@ -13,17 +14,33 @@ const initialState= {
         {id: v1(), message: "Hi", likesCount: 25},
     ],
     newPostText: '',
+    profile: null,
 }
+
+
+type setUserProfileActionType = {
+    type: 'SET_USER_PROFILE'
+    profile: any
+}
+export type ActionsTypes =
+    AddPostActionType
+    | UpdateNewPostTextActionType
+    | AddMessageActionType
+    | setUserProfileActionType
+
 
 export const profileReducer = (state: ProfilePageType = initialState, action: ActionsTypes): ProfilePageType => {
 
     switch (action.type) {
-        case 'ADD-POST': {
+        case ADD_POST: {
             let newPost = {id: v1(), message: state.newPostText, likesCount: 0};
             return {...state, posts: [...state.posts, newPost], newPostText: ''};
         }
-        case 'UPDATE-NEW-POST': {
+        case UPDATE_NEW_POST: {
             return {...state, newPostText: action.newText};
+        }
+        case SET_USER_PROFILE: {
+            return {...state, profile: action.profile}
         }
         default:
             return state;
@@ -38,3 +55,12 @@ export const addPostAC = (): AddPostActionType => {
 export const updateNewPostAC = (text: string): UpdateNewPostTextActionType => {
     return {type: UPDATE_NEW_POST, newText: text}
 }
+
+export const setUserProfile = (profile: any): setUserProfileActionType => {
+    return {type: SET_USER_PROFILE, profile}
+}
+
+
+
+
+
