0bd3495fe2b5ec94cd14cd48161cecb068918bc4
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-29 13:30:29 +0300

    feat: add lesson#59 change structure

--- src/Redux/profile-reducer.test.ts
+++ src/Redux/profile-reducer.test.ts
@@ -1,29 +1,42 @@
 import { v1 } from "uuid";
-import {addPostAC, profileReducer, updateNewPostAC} from "./profile-reducer";
-
-
+import { profileReducer, addPostAC, updateNewPostAC, setUserProfile } from "./profile-reducer";
 
 describe("profileReducer", () => {
-    let state = {
+    let initialState = {
         posts: [
-            { id: v1(), message: "Post 1", likesCount: 0 },
-            { id: v1(), message: "Post 2", likesCount: 0 },
+            { id: v1(), message: "Hi", likesCount: 15 },
+            { id: v1(), message: "How is your it-kamasutra", likesCount: 14 },
+            { id: v1(), message: "yo", likesCount: 9 },
+            { id: v1(), message: "полный пипец", likesCount: 10 },
+            { id: v1(), message: "Hi", likesCount: 25 },
         ],
         newPostText: "",
+        profile: null,
     };
 
     it("should add a new post to the state", () => {
+        const text = "New post";
         const action = addPostAC();
-        const newState = profileReducer(state, action);
-        expect(newState.posts.length).toBe(3);
-        expect(newState.posts[2].message).toBe("");
-        expect(newState.posts[2].likesCount).toBe(0);
+        const newState = profileReducer(initialState, action);
+
+        expect(newState.posts.length).toBe(initialState.posts.length + 1);
+        expect(newState.posts[newState.posts.length - 1].message).toBe("");
+        expect(newState.posts[newState.posts.length - 1].likesCount).toBe(0);
     });
 
     it("should update the new post text in the state", () => {
-        const newText = "New post text";
-        const action = updateNewPostAC(newText);
-        const newState = profileReducer(state, action);
-        expect(newState.newPostText).toBe(newText);
+        const text = "New post text";
+        const action = updateNewPostAC(text);
+        const newState = profileReducer(initialState, action);
+
+        expect(newState.newPostText).toBe(text);
+    });
+
+    it("should set the user profile in the state", () => {
+        const profile = { name: "John Doe", age: 25 };
+        const action = setUserProfile(profile);
+        const newState = profileReducer(initialState, action);
+
+        expect(newState.profile).toBe(profile);
     });
 });
