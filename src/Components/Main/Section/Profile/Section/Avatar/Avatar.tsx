521bac5a12ecc35b3425fd41339d19e636cabc41
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-29 13:29:19 +0300

    feat: add lesson#59 change structure

--- src/Components/Main/Section/Profile/Section/Avatar/Avatar.tsx
+++ src/Components/Main/Section/Profile/Section/Avatar/Avatar.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {ProfileType} from "../../Profile";
 
-export const Avatar = () => {
+
+type AvatarProps = {
+    profile: ProfileType;
+};
+
+export const Avatar: React.FC<AvatarProps> = ({ profile }) => {
     return (
         <div>
+            <img src={profile.photos.large} alt="logo user" />
             Avatar Desc
         </div>
     );
 };
-
