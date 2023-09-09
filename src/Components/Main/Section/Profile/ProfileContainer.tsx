54c44c7b32ecfca4a09f6999acf13a618ce95b42
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-09 15:55:12 +0300

    feat:lesson#60 change structure ProfileContainer

--- src/Components/Main/Section/Profile/ProfileContainer.tsx
+++ src/Components/Main/Section/Profile/ProfileContainer.tsx
@@ -1,41 +1,58 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import profile from './Profile.module.css'
 import {Profile, ProfileType} from "./Profile";
 import axios from "axios";
-import { connect } from "react-redux";
-import { setUserProfile} from "../../../../Redux/profile-reducer";
-import { AppRootState } from "../../../../Redux/redux-store";
+import {connect} from "react-redux";
+import {setUserProfile} from "../../../../Redux/profile-reducer";
+import {AppRootState} from "../../../../Redux/redux-store";
+import {RouteComponentProps, withRouter} from "react-router-dom";
+import {StateType} from "../../../../Redux/type";
+
+type PathParamsType = {
+    userId: string
+}
 
 type MapStateToPropsType = {
     profile: ProfileType | null
 }
 
 type MapDispatchPropsType = {
-    setUserProfile: (profile: ProfileType) => void
+    setUserProfile: (profile: ProfileType | null) => void
 }
 
 type ProfileAPIContainerPropsType = MapStateToPropsType & MapDispatchPropsType;
 
-class ProfileAPIContainer extends React.Component<ProfileAPIContainerPropsType> {
-    componentDidMount() {
-        axios.get(
-            `https://social-network.samuraijs.com/api/1.0/profile/2`
-        ).then((response) => {
-            this.props.setUserProfile(response.data);
-        });
-    }
+type CommonPropsType = RouteComponentProps<PathParamsType> & ProfileAPIContainerPropsType
 
-    render() {
-        return (
-            <div className={profile.body}>
-                <Profile profile={this.props.profile} />
-            </div>
-        );
-    }
+
+const ProfileContainer = (props: CommonPropsType) => {
+
+    useEffect(() => {
+        let userId = props.match.params.userId;
+        if (!userId) {
+            userId = '2';
+        }
+        axios.get(
+            `https://social-network.samuraijs.com/api/1.0/profile/${userId}`
+        ).then((response) => {
+            props.setUserProfile(response.data);
+        });
+    }, [])
+
+    return (
+        <div className={profile.body}>
+            <Profile profile={props.profile}/>
+        </div>
+    );
 }
 
 let mapStateToProps = (state: AppRootState): MapStateToPropsType => ({
     profile: state.profilePage.profile,
 });
 
-export const ProfileContainer = connect(mapStateToProps, { setUserProfile })(ProfileAPIContainer);
+let withUrlDataContainerComponent: any = withRouter(ProfileContainer)
+
+export default connect<MapStateToPropsType,MapDispatchPropsType,CommonPropsType,AppRootState>(mapStateToProps, {setUserProfile})(withUrlDataContainerComponent);
+
+
+//типизация коннекта ох не факт что правильно!!!!!!!
