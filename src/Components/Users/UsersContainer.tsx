09a2fc86a94c3c19545eacd7acafe9ea79d893f9
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-09 18:33:09 +0300

    feat: lesson#62 change structure

--- src/Components/Users/UsersContainer.tsx
+++ src/Components/Users/UsersContainer.tsx
@@ -60,10 +60,7 @@ export class UsersAPIComponent extends Component<UserType> {
     };
 
     requestUsers = (page: number) => {
-        axios
-            .get(
-                `https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`
-            )
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`, {withCredentials: true})
             .then((response) => {
                 this.props.toggleIsFetching(false)
                 this.props.setUsers(response.data.items);
