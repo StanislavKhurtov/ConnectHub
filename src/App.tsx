d32ff61d6c2fb8e814948a4762760cb04b6702ac
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-09 17:48:56 +0300

    feat: lesson#61 change structure app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./Components/Header/Header";
 import {Main} from "./Components/Main/Main";
 import {Footer} from "./Components/Footer/Footer";
+import HeaderContainer from "./Components/Header/HeaderContainer";
 
 
 export const App = () => {
     return (
         <div className="app-wrapper">
-            <Header />
+            <HeaderContainer />
             <Main />
             <Footer />
         </div>
