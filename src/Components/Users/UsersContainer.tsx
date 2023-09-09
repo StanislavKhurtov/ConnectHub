23e6eef4e14f9a6d35c75d2c229dadceb26c310f
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-09 19:54:13 +0300

    feat:  lesson#63 change structure UsersContainer

--- src/Components/Users/UsersContainer.tsx
+++ src/Components/Users/UsersContainer.tsx
@@ -10,10 +10,9 @@ import {
     unFollow,
     UsersPageType
 } from "../../Redux/users-reducer";
-
-import axios from "axios";
 import {Users} from "./Users";
 import {Preloader} from "../common/Preloader/Preloader";
+import {userAPI} from "../api/api";
 
 type UserType = {
     users: Array<UsersPageType>;
@@ -60,11 +59,11 @@ export class UsersAPIComponent extends Component<UserType> {
     };
 
     requestUsers = (page: number) => {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`, {withCredentials: true})
-            .then((response) => {
+        userAPI.getUsers(page, this.props.pageSize)
+            .then((data) => {
                 this.props.toggleIsFetching(false)
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount);
+                this.props.setUsers(data.items);
+                this.props.setTotalUsersCount(data.totalCount);
             });
     };
 
