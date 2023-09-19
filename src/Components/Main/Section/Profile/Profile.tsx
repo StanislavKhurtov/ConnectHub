7d8f56e926304a5eb352d7b8719bcaa0cb268839
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-19 21:45:09 +0300

    refactor: lesson 71 - 74 change structure project

--- src/Components/Main/Section/Profile/Profile.tsx
+++ src/Components/Main/Section/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MyPostsContainer } from './Section/MyPosts/MyPostsContainer';
 import { ProfileInfo } from './Section/ProfileInfo/ProfileInfo';
+import {ProfileStatus} from "./Section/ProfileInfo/ProfileStatus";
 
 export type ProfileType= {
     aboutMe: string;
@@ -26,12 +27,15 @@ export type ProfileType= {
 
 export type ProfileProps = {
     profile: ProfileType | null;
+    status: string
+    onChange: (newValue: string) => void
 };
 
-export const Profile: React.FC<ProfileProps> = ({ profile }) => {
+export const Profile: React.FC<ProfileProps> = ({ profile, status, onChange }) => {
     return (
         <div >
             <ProfileInfo profile={profile} />
+            <ProfileStatus status={status} onChange={onChange}/>
             <MyPostsContainer />
         </div>
     );
