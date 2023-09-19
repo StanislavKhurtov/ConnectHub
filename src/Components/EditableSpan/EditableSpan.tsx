93ca64cb40678472299b27d0b49f6877aefffa9a
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-19 21:44:57 +0300

    refactor: lesson 71 - 74 change structure project add EditableSpan

---
+++ src/Components/EditableSpan/EditableSpan.tsx
@@ -0,0 +1,37 @@
+import React, {ChangeEvent, useState} from "react";
+
+
+type EditableSpanType = {
+    title: string
+    onChange: (newValue: string) => void
+}
+export const EditebleSpan = ((props: EditableSpanType) => {
+
+    let [editMode, setEditMode] = useState(false)
+    let [newTitle, setNewTitle] = useState("")
+
+    const activeEditMode = () => {
+        setEditMode(true)
+        setNewTitle(props.title)
+    };
+    const activeViewMode = () => {
+        setEditMode(false)
+        props.onChange(newTitle);
+
+    };
+
+    const onChangeTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewTitle(e.currentTarget.value)
+    }
+
+    return (
+        editMode
+            ? <input
+                autoFocus
+                value={newTitle}
+                onChange={onChangeTitleHandler}
+                onBlur={activeViewMode}
+            />
+            : <span onClick={activeEditMode}>{props.title}</span>
+    );
+})
