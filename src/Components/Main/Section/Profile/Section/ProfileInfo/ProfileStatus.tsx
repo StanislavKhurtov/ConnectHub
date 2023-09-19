01ab1d46057d7339a822591a8f159fed664f2087
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-19 21:46:40 +0300

    refactor: lesson 71 - 74 change structure project

---
+++ src/Components/Main/Section/Profile/Section/ProfileInfo/ProfileStatus.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {EditebleSpan} from "../../../../../EditableSpan/EditableSpan";
+
+type ProfileStatusType = {
+    status: string
+    onChange: (newValue: string) => void
+}
+export const ProfileStatus = (props: ProfileStatusType) => {
+
+    return (
+        <div>
+            <EditebleSpan title={props.status} onChange={props.onChange}/>
+        </div>
+    );
+};
+
