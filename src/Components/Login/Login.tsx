6f8a3b69a12db1469d4413df60fcabdb77a38fa8
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-22 19:01:37 +0300

    feat: lesson 75 add login form

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, {FormEventHandler} from 'react';
+import {Field, reduxForm} from "redux-form";
 
 type LoginType = {}
 
 export const Login = (props: LoginType) => {
-
+    const onSubmit = (formData: any) => {
+        console.log(formData)
+    }
     return (
         <div>
-            Login
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
         </div>
     );
 };
+
+
+type LoginFormType = {
+    handleSubmit: FormEventHandler<HTMLFormElement> | undefined
+}
+export const LoginForm = (props: LoginFormType) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={'Login'} component={'input'} name={"login"}/>
+            </div>
+            <div>
+                <Field placeholder={'Password'} component={'input'} name={"password"}/>
+            </div>
+            <div>
+                <Field component={'input'} name={"rememberMe"} type='checkbox'/>Remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    );
+};
+
+const LoginReduxForm = reduxForm({
+    form: 'login',
+})(LoginForm)
+
+
 
