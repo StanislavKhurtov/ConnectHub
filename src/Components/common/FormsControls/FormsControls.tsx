d139408d13e7d46de00b9d0c91ac563dd4ac1c13
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-23 23:13:47 +0300

    feat: lesson 77 change structure

---
+++ src/Components/common/FormsControls/FormsControls.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import s from './FormsControls.module.css'
+
+
+export const Textarea = ({input, meta, ...restProps}: any) => {
+    return (<FormControl children={<textarea {...input} {...restProps} />} meta={meta}/>)
+}
+
+export const Input = ({input, meta, ...restProps}: any) => {
+    return (<FormControl children={<input {...input} {...restProps}/>} meta={meta}/>)
+}
+export const FormControl = ({meta, children}: any) => {
+    const hasError = meta.touched && meta.error
+    return (
+        <div className={s.formControl + " " + (hasError ? s.error : '')}>
+            <div>{children}</div>
+            <div>{hasError && <span className='s.error'>{meta.error}</span>}</div>
+        </div>
+    )
+}
