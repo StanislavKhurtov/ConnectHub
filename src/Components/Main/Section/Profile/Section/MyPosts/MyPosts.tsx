be63ec164030b945fb8c62f07df05d4912babc94
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-23 20:49:27 +0300

    feat: lesson 76 change structure

--- src/Components/Main/Section/Profile/Section/MyPosts/MyPosts.tsx
+++ src/Components/Main/Section/Profile/Section/MyPosts/MyPosts.tsx
@@ -1,49 +1,25 @@
-import React, {ChangeEvent, KeyboardEvent, MouseEvent} from 'react';
+import React, {FormEventHandler} from 'react';
 import mp from './MyPost.module.css';
 import Post from "./Post/Post";
 import {PostType} from "../../../../../../Redux/type";
-
+import {Field, reduxForm} from "redux-form";
 
 type MyPostType = {
     posts: PostType[]
-    newPostText: string
-    addPost: () => void
-    updateNewPostText: (text: string) => void
+    addPost: (values: any) => void
 }
 
 export const MyPosts = (props: MyPostType) => {
 
-    const onAddPost = (e: MouseEvent<HTMLButtonElement>) => {
-        props.addPost()
-    }
-
-    const onPostChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        let text = e.currentTarget.value;
-        if (text.trim() !=='') {
-            props.updateNewPostText(text)
-        }
-    }
-
-    const onKeyPressHandler = (e: KeyboardEvent<HTMLTextAreaElement>) => {
-        if (e.key === 'Enter') {
-            props.addPost()
-        }
+    const onAddPost = (values: any) => {
+        props.addPost(values.newPostText)
     }
 
 
     return (
         <div className={mp.wrapper}>
             <div>My Post</div>
-            <div>
-                <div>
-                    <textarea value={props.newPostText === '' ? '' : props.newPostText} onChange={onPostChange}
-                              onKeyPress={onKeyPressHandler}/>
-                </div>
-                <div>
-                    <button onClick={onAddPost}>Add post</button>
-                </div>
-            </div>
-
+            <AddNewPostFormRedux onSubmit={onAddPost}/>
             <div className={mp.posts}>{props.posts.map((el: any) => {
                 return (
                     <Post key={el.id} id={el.id} message={el.message} like={el.likesCount}/>
@@ -53,3 +29,20 @@ export const MyPosts = (props: MyPostType) => {
         </div>
     );
 };
+
+type AddNewProFormType = {
+    handleSubmit: FormEventHandler<HTMLFormElement> | undefined
+}
+
+const AddNewPostForm = (props: AddNewProFormType) => {
+    return <form onSubmit={props.handleSubmit}>
+        <div>
+            <Field name='newPostText' component={'textarea'}/>
+        </div>
+        <div>
+            <button>Add post</button>
+        </div>
+    </form>;
+}
+
+const AddNewPostFormRedux = reduxForm({form: 'ProfileAddNewPostForm'})(AddNewPostForm)
